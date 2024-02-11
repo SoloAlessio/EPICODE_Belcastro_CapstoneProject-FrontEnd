c7a98c679eec032dca18ed47a5d49414410cce71
Alessio <belcastroalessio@outlook.it>
2024-02-11 23:38:38 +0100

    trying with URL string manipolation

--- app/account/callbackPage/page.jsx
+++ app/account/callbackPage/page.jsx
@@ -7,18 +7,19 @@ export default function Page() {
     const router = useRouter()
 
     useEffect(() => {
-        if (router && router.query) {
-            const token = router.query.token
-            if (token) {
-                console.log(token)
-                localStorage.setItem(`token`, token)
-                router.push(`/dashboard`)
-            } else {
-                alert(`OPS, Something went wrong!`)
-                router.push(`/`)
-            }
+        const url = window.location.href
+        const index = url.indexOf(`?`)
+
+        const token = url.substring(index)
+
+        if (token) {
+            localStorage.setItem(`token`, token)
+            router.push(`/dashboard`)
+        } else {
+            alert(`OPS, Something went wrong!`)
+            router.push(`/`)
         }
-    }, [router])
+    }, [url])
 
     return (
         <div className='flex h-[700px] items-center justify-center'>
