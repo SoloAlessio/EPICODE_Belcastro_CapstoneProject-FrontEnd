56f1945c1556dde2003fba1fd6aaa6a98e070d1b
Alessio <belcastroalessio@outlook.it>
2024-02-11 23:28:39 +0100

    nextJs typError fixing

--- app/account/callbackPage/page.jsx
+++ app/account/callbackPage/page.jsx
@@ -7,16 +7,18 @@ export default function Page() {
     const router = useRouter()
 
     useEffect(() => {
-        const token = router.query.token ?? ''
-        if (token) {
-            console.log(token)
-            localStorage.setItem(`token`, token)
-            router.push(`/dashboard`)
-        } else {
-            alert(`OPS, Something went wrong!`)
-            router.push(`/`)
+        if (router && router.query) {
+            const token = router.query.token
+            if (token) {
+                console.log(token)
+                localStorage.setItem(`token`, token)
+                router.push(`/dashboard`)
+            } else {
+                alert(`OPS, Something went wrong!`)
+                router.push(`/`)
+            }
         }
-    }, [router.query])
+    }, [router])
 
     return (
         <div className='flex h-[700px] items-center justify-center'>
